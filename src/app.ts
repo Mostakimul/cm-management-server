549119c3adadb659d771fd58094c7e4fc4281cdd
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-05-21 02:55:23 -0400

    [feat]: origin updated

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import notFound from './app/middlewares/notFound'
-import router from './app/routes'
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://cmu-client.vercel.app/', '*'],
     credentials: true,
   }),
-)
+);
 
 // application routes
-app.use('/api/v1', router)
+app.use('/api/v1', router);
 
 const test = (req: Request, res: Response) => {
-  res.send('Hello World!')
-}
-app.get('/', test)
+  res.send('Hello World!');
+};
+app.get('/', test);
 
-app.use(globalErrorHandler)
-app.use(notFound)
+app.use(globalErrorHandler);
+app.use(notFound);
 
-export default app
+export default app;
